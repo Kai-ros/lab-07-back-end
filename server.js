c086695f47e8d3a131afe790fe056cd78da52cc5
Promila Tamanggit config --global user.email promilatam@gmail.com <promilatam@gmail.com>
2019-07-11 12:19:23 -0700

    promila/change

--- server.js
+++ server.js
@@ -3,36 +3,84 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
+const superagent = require('superagent');
+
 
 // Global variables
 const PORT = process.env.PORT;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
 const EVENTBRITE_API_KEY = process.env.EVENTBRITE_API_KEY;
+const client = new pg.Client(process.env.DATABASE_URL);
+
+client.connect();
+client.on('error',
+  error => {
+    console.error(error);
+  })
+
 
 // Construct server with dependency objects
 const app = express();
 app.use(cors());
-const superagent = require('superagent');
 
 // Use express to get location data
 app.get('/location', searchToLatLng);
+
+// function to check DB for existing table
+const existsInDB = function(tableName, data) {
+  let queryString = `SELECT * FROM ${tableName} WHERE search_input=$1`, 
+
+    return client.query(queryString, [data])
+    .then(sqlResult => { 
+      if (sqlResult.rowCount === 0) { return false; } else { return true; };
+    });
+  
+};
 
 // Performs task of building object from JSON file
 function searchToLatLng(request, response) {
   const locationName = request.query.data;
   const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=${GEOCODE_API_KEY}`;
+  const tableName = locations;
 
-  superagent.get(geocodeURL)
-    .then(result => {
-      let location = new LocationConstructor(result.body);
-      location.search_query = locationName;
+  const tableExists = existsInDB(tableName, locationName);
+  
+  client.query(`SELECT * FROM locations WHERE search_query=$1`, [locationName])
+    .then(sqlResult => {
 
-      response.send(location);
-    }).catch(error => {
-      console.error(error);
-      response.status(500).send('Status 500: Life is hard mang.');
-    })
+      if(tableExists) {
+        console.log('retrieving from google');
+
+        superagent.get(geocodeURL)
+          .then(result => {
+
+            let location = new LocationConstructor(result.body);
+            
+            client.query(
+              `INSERT INTO locations(
+                search_input,
+                search_query, 
+                formatted_query,
+                latitude,
+                longitude
+              ) VALUES ($1, $2, $3, $4, $5)`,
+              [location.search_input, location.search_query, location.formatted_query, location.latitude, location.longitude]
+            )
+    
+            response.send(location);
+
+          }).catch(error => {
+            console.error(error);
+            response.status(500).send('Status 500: Life is hard mang.');
+          })
+      } else{
+        console.log('sent from DB');
+        response.send(sqlResult.row[0]);
+      }
+    });
+
 }
 
 // constructor function to build weather objects
